e9a5bdc0b31c0f94de0572fca0b78ef7daf25696
gabrom212 <gabrom212@gmail.com>
2025-05-17 12:23:15 -0400

    Vanilla explosion!
    Vanilla explosion implemented for ships. This works but it needs to use the CannonBallExplosion because. there are no effects and the explosion is too big for the cannonball.

--- src/main/java/net/bigger212/vs2_sup_fix/mixin/CannonBallEntityPatch.java
+++ src/main/java/net/bigger212/vs2_sup_fix/mixin/CannonBallEntityPatch.java
@@ -1,33 +1,63 @@
 package net.bigger212.vs2_sup_fix.mixin;
 
 import net.mehvahdjukaar.supplementaries.common.entities.CannonBallEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.core.impl.game.ships.ShipData;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 
 @Mixin(CannonBallEntity.class)
 public class CannonBallEntityPatch {
 
-    @Inject(method = "maybeBounce", at = @At("HEAD"), cancellable = true)
-    @SuppressWarnings({"UnreachableCode"})
-    private void injectMaybeBounce(BlockHitResult hit, CallbackInfoReturnable<Boolean> cir) {
-        World world = ((CannonBallEntity) (Object) this).getWorld();
+//    @Inject(method = "maybeBounce", at = @At("HEAD"), cancellable = true)
+//    @SuppressWarnings({"UnreachableCode"})
+//    private void injectMaybeBounce(BlockHitResult hit, CallbackInfoReturnable<Boolean> cir) {
+//        World world = ((CannonBallEntity) (Object) this).getWorld();
+//
+//        Ship ship = VSGameUtilsKt.getShipObjectManagingPos(world, hit.getBlockPos());
+//
+//        if (ship != null) {
+//            // If on ship, cancel bounce by returning false immediately.
+//            // This is done for version 1.1 because the cannonball seems to
+//            // bounce through ships -leaving them unaffected though detected.
+//            cir.setReturnValue(false);
+//            cir.cancel();
+//        }
+//    }
 
-        Ship ship = VSGameUtilsKt.getShipObjectManagingPos(world, hit.getBlockPos());
+    @Inject(method = "onBlockHit", at = @At("HEAD"), cancellable = true)
+    @SuppressWarnings("UnreachableCode")
+    private void injectOnBlockHit(BlockHitResult result, CallbackInfo ci) {
+        Vec3d hitPos = result.getPos();
+        World world = ((CannonBallEntity)(Object) this).getWorld();
+
+        if (!(world instanceof ServerWorld serverWorld)) return;
+
+        Ship ship = VSGameUtilsKt.getShipObjectManagingPos(world, result.getBlockPos());
 
         if (ship != null) {
-            // If on ship, cancel bounce by returning false immediately.
-            // This is done for version 1.1 because the cannonball seems to
-            // bounce through ships leaving -them unaffected though detected.
-            cir.setReturnValue(false);
-            cir.cancel();
+            Explosion explosion = new Explosion(world, null,
+                    world.getDamageSources().explosion(null), null,
+                    hitPos.x, hitPos.y, hitPos.z,
+                    4.0f, false, Explosion.DestructionType.DESTROY);
+
+            explosion.collectBlocksAndDamageEntities();
+            explosion.affectWorld(true);
+            // This works but it needs to use the CannonBallExplosion because
+            // there are no effects and the explosion is too big for the cannonball.
+            // Also, I dont know why it is bouncing again -seemingly properly this time. /shrug.
+            ci.cancel();
         }
     }
 }
--- src/main/java/net/bigger212/vs2_sup_fix/mixin/CannonBallExplosionPatch.java
+++ src/main/java/net/bigger212/vs2_sup_fix/mixin/CannonBallExplosionPatch.java
@@ -1,22 +1,46 @@
 package net.bigger212.vs2_sup_fix.mixin;
 
+import com.google.common.collect.Multiset;
+import net.mehvahdjukaar.supplementaries.SuppPlatformStuff;
 import net.mehvahdjukaar.supplementaries.common.misc.explosion.CannonBallExplosion;
+import net.minecraft.block.AbstractFireBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import net.minecraft.world.explosion.Explosion;
+import org.jetbrains.annotations.Nullable;
+import org.joml.RoundingMode;
+import org.joml.Vector3d;
+import org.joml.Vector3i;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.core.api.ships.properties.ChunkClaim;
+import org.valkyrienskies.core.api.world.ServerShipWorld;
+import org.valkyrienskies.core.api.world.ShipWorld;
+import org.valkyrienskies.core.impl.game.ships.ShipData;
 import org.valkyrienskies.mod.common.CompatUtil;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
@@ -24,4 +48,46 @@ import java.util.concurrent.atomic.AtomicReference;
 @Mixin(CannonBallExplosion.class)
 public class CannonBallExplosionPatch {
 // TODO Get the explosion to work on ships!
+
+
+//    @Inject(method = "destroyBlockNoEffects", at = @At("HEAD"), cancellable = true)
+//    private void onDestroyBlockVSCompatible(BlockPos pos, World level, @Nullable Entity entity, int recursionLeft, Multiset<SoundEvent> sounds, CallbackInfoReturnable<Boolean> cir) {
+//        if (!(level instanceof ServerWorld serverWorld)) return;
+//
+//        BlockState blockState = level.getBlockState(pos);
+//        if (blockState.isAir()) {
+//            cir.setReturnValue(false);
+//            return;
+//        } else {
+//
+//            Ship ship = VSGameUtilsKt.getShipObjectManagingPos(level, pos);
+//
+//            if (ship == null) return;
+//
+//            Vector3d transformed = ship.getWorldToShip().transformPosition(VectorConversionsMCKt.toJOMLD(pos));
+//
+//            BlockPos newpos = VectorConversionsMCKt.toBlockPos(new Vector3i(
+//                    (int) Math.floor(transformed.x),
+//                    (int) Math.floor(transformed.y),
+//                    (int) Math.floor(transformed.z)
+//            ));
+//
+//            System.out.println("rerouting explosion to pos: " + pos);
+//
+//            FluidState fluidState = level.getFluidState(pos);
+//            if (level.isClient && !(blockState.getBlock() instanceof AbstractFireBlock)) {
+//                sounds.add(SuppPlatformStuff.getSoundType(blockState, pos, level, entity).getBreakSound());
+//                level.addBlockBreakParticles(pos, blockState);
+//            }
+//
+//            BlockEntity blockEntity = blockState.hasBlockEntity() ? level.getBlockEntity(pos) : null;
+//            Block.dropStacks(blockState, level, pos, blockEntity, entity, ItemStack.EMPTY);
+//            boolean bl = level.setBlockState(pos, fluidState.getBlockState(), 3, recursionLeft);
+//            if (bl) {
+//                level.emitGameEvent(GameEvent.BLOCK_DESTROY, pos, GameEvent.Emitter.of(entity, blockState));
+//            }
+//
+//            cir.setReturnValue(bl);
+//        }
+//    }
 }
