f27f5337fc9b14fafe292ce54b78f4b2105154f1
gabrom212 <gabrom212@gmail.com>
2025-05-17 14:18:30 -0400

    Explosion @ ship pos
    This works but it needs to use the CannonBallExplosion because there are no effects.
    Also removed bounce check.

--- src/main/java/net/bigger212/vs2_sup_fix/mixin/CannonBallEntityPatch.java
+++ src/main/java/net/bigger212/vs2_sup_fix/mixin/CannonBallEntityPatch.java
@@ -1,6 +1,7 @@
 package net.bigger212.vs2_sup_fix.mixin;
 
 import net.mehvahdjukaar.supplementaries.common.entities.CannonBallEntity;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.Box;
@@ -20,23 +21,7 @@ import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 @Mixin(CannonBallEntity.class)
 public class CannonBallEntityPatch {
 
-//    @Inject(method = "maybeBounce", at = @At("HEAD"), cancellable = true)
-//    @SuppressWarnings({"UnreachableCode"})
-//    private void injectMaybeBounce(BlockHitResult hit, CallbackInfoReturnable<Boolean> cir) {
-//        World world = ((CannonBallEntity) (Object) this).getWorld();
-//
-//        Ship ship = VSGameUtilsKt.getShipObjectManagingPos(world, hit.getBlockPos());
-//
-//        if (ship != null) {
-//            // If on ship, cancel bounce by returning false immediately.
-//            // This is done for version 1.1 because the cannonball seems to
-//            // bounce through ships -leaving them unaffected though detected.
-//            cir.setReturnValue(false);
-//            cir.cancel();
-//        }
-//    }
-
-    @Inject(method = "onBlockHit", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "onBlockHit", at = @At("HEAD"))
     @SuppressWarnings("UnreachableCode")
     private void injectOnBlockHit(BlockHitResult result, CallbackInfo ci) {
         Vec3d hitPos = result.getPos();
@@ -50,14 +35,13 @@ public class CannonBallEntityPatch {
             Explosion explosion = new Explosion(world, null,
                     world.getDamageSources().explosion(null), null,
                     hitPos.x, hitPos.y, hitPos.z,
-                    4.0f, false, Explosion.DestructionType.DESTROY);
+                    2.5f,
+                    false, Explosion.DestructionType.DESTROY_WITH_DECAY);
 
             explosion.collectBlocksAndDamageEntities();
-            explosion.affectWorld(true);
+            explosion.affectWorld(false);
             // This works but it needs to use the CannonBallExplosion because
-            // there are no effects and the explosion is too big for the cannonball.
-            // Also, I dont know why it is bouncing again -seemingly properly this time. /shrug.
-            ci.cancel();
+            // there are no effects.
         }
     }
 }
